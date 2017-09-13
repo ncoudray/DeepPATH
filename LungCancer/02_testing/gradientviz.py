df791120ce6d8c100a86744cb81651d1fe82ea52
kocharshaivi19 <sk6435@nyu.edu>
2017-09-13 13:20:41 -0400

    Bug fixes for Var_grad

--- LungCancer/02_testing/gradientviz.py
+++ LungCancer/02_testing/gradientviz.py
@@ -8,6 +8,8 @@ from inception.nc_imagenet_data import ImagenetData
 from inception.inception_model import inference
 import os
 import cv2
+import pdb
+
 FLAGS = None
 
 def outer_grad_inception(labels, batch_size=1, learningrate=0.1, weight_decay=0.1, num_classes =1):
@@ -26,20 +28,9 @@ def outer_grad_inception(labels, batch_size=1, learningrate=0.1, weight_decay=0.
     print image.shape
     x = tf.placeholder(tf.float32, [1, image.shape[1], image.shape[2], image.shape[3]])
     print x.shape
-    y = tf.placeholder(tf.int32)
-
-
-    sparse_labels = tf.reshape(y, [batch_size, 1])
-    print ("sparse: ", sparse_labels)
-    indices = tf.reshape(tf.range(batch_size), [batch_size, 1])
-    print ("ind: ", indices)
-    concated = tf.concat(axis=1, values=[indices, sparse_labels])
-    # num_classes = logits[0].get_shape()[-1].value
-    dense_labels = tf.sparse_to_dense(concated,
-                                      [batch_size, num_classes],
-                                      1.0, 0.0)
-    logits, auxiliary_logits, endpoints, net2048 = inference(x, 1)
-
+    y = tf.placeholder(tf.int32, [1])
+    label = tf.one_hot(y, 2)
+    logits, auxiliary_logits, endpoints, net2048 = inference(x, 2)
     with tf.get_default_graph() and tf.Session(config=tf.ConfigProto(allow_soft_placement=True))as sess:
         saver = tf.train.import_meta_graph("{}.meta".format(checkpoint))
         saver.restore(sess, checkpoint)
@@ -50,24 +41,17 @@ def outer_grad_inception(labels, batch_size=1, learningrate=0.1, weight_decay=0.
         sess.run(tf.global_variables_initializer())
 
         print "endpoints", endpoints
-
-        # loss_grad = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='tower_0/CrossEntropyLoss/value/avg')[0]
         loss_grad = slim.losses.cross_entropy_loss(logits,
-                                 dense_labels,
+                                 label,
                                  label_smoothing=0.1,
                                  weight=1.0)
 
         print "loss_grad", loss_grad
-
         var_grad = tf.gradients(loss_grad, [x])
-
         print "var_gard", var_grad
-
-        print "sess var: ", sess.run(var_grad, feed_dict={x: image, y: 1})
-
-
+        print "sess var: ", sess.run(var_grad, feed_dict={x: image, y: np.array([1])})
         for i in range(10):
-            x_grad = sess.run(var_grad, feed_dict={x: image, y: 1})
+            x_grad = sess.run(var_grad, feed_dict={x: image, y: np.array([1])})
             print x_grad
             # image -= (x_grad[0] * learningrate + weight_decay * learningrate * image)
             # print image
