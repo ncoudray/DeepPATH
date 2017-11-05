5feb0ea5169ecd05e159f2318ff40a098e8f60d7
shaivikochar <sk6435@nyu.edu>
2017-11-05 01:33:55 -0500

    Resize Error

--- LungCancer/02_testing/dcgan_imagenet.py
+++ LungCancer/02_testing/dcgan_imagenet.py
@@ -23,7 +23,7 @@ def merge(images, size):
     print ("size: ", size)
     h, w = images.shape[1], images.shape[2]
     print ("h: {0}, w: {1} ".format(h, w))
-    img = np.zeros([h * size[0], w * size[1], images.shape[2]])
+    img = np.zeros([h * size[0], w * size[1], images.shape[3]])
     print ("img:", img.shape)
     for idx, image in enumerate(images):
         i = int(idx % size[1])
