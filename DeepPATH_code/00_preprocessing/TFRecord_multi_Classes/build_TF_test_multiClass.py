e2a273959016ca4b03fb6c0ce6812e2188ea7912
ncoudray <coudrn01@phoenix2.cm.cluster>
2018-01-09 15:28:01 -0500

    3.6 read(r) to read(rd) - python 36 compatible

--- DeepPATH_code/00_preprocessing/TFRecord_multi_Classes/build_TF_test_multiClass.py
+++ DeepPATH_code/00_preprocessing/TFRecord_multi_Classes/build_TF_test_multiClass.py
@@ -168,7 +168,7 @@ def _process_image(filename, coder):
     width: integer, image width in pixels.
   """
   # Read the image file.
-  with tf.gfile.FastGFile(filename, 'r') as f:
+  with tf.gfile.FastGFile(filename, 'rb') as f:
     image_data = f.read()
   #image_data = tf.gfile.FastGFile(filename, 'r').read()
 
