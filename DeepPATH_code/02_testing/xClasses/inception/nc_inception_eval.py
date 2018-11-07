1b96cc34c8c2df3789d617762fa5f24ef269abc0
ncoudray <coudrn01@phoenix2.cm.cluster>
2018-11-07 11:30:30 -0500

    do not save net2048

--- DeepPATH_code/02_testing/xClasses/inception/nc_inception_eval.py
+++ DeepPATH_code/02_testing/xClasses/inception/nc_inception_eval.py
@@ -247,7 +247,7 @@ def _eval_once(saver, summary_writer, top_1_op, top_5_op, summary_op, max_percen
                 output_tmp.close()
 
         # save last-but one layer
-          if True:
+          if False:
             print("net2048")
             print(net2048)
             net2048_path = os.path.join(FLAGS.eval_dir, 'net2048')
