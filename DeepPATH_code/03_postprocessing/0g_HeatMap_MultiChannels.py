a9fdd8b06d0401585c70800ecb1219abf69bb3db
Coudray <coudrn01@bigpurple-ln4.cm.cluster>
2019-11-15 17:18:10 -0500

    speed up heatmap and allow for multiple stats files

--- DeepPATH_code/03_postprocessing/0g_HeatMap_MultiChannels.py
+++ DeepPATH_code/03_postprocessing/0g_HeatMap_MultiChannels.py
@@ -33,9 +33,11 @@ FLAGS = None
 
 
 
-def dict_tiles_stats():
+def dict_tiles_stats(filepath):
 	stats_dict = {}
-	with open(FLAGS.tiles_stats) as f:
+	if filepath == '':
+		return stats_dict
+	with open(filepath) as f:
 		for line in f:
 			line2 = line.replace('[','').replace(']','').split()
 			if len(line2)>0:	
@@ -49,7 +51,6 @@ def dict_tiles_stats():
 
 
 def get_inference_from_file(TileName, stats_dict):
-
 	# print("basename is :" + basename)
 	current_score = -1
 	score_correction = 0
@@ -61,9 +62,8 @@ def get_inference_from_file(TileName, stats_dict):
 		lineProb = lineProb.split(']')[0]
 		lineProb = lineProb.split()
 		line = line.replace('[','').replace(']','').split()
-		print(line)
-		print(lineProb)
-	
+		# print(line)
+		# print(lineProb)
 		is_TP = line[1]
 		NumberOfClasses = len(lineProb)
 		class_all = []
@@ -75,10 +75,7 @@ def get_inference_from_file(TileName, stats_dict):
 			class_all[nC] = class_all[nC] / sum_class
 		current_score = max(class_all)
 		oClass = class_all.index(max(class_all)) + 1
-
-
 		score_correction = 1.0 / len(class_all)
-		
 		'''analyze = True
 		if os.path.isfile(FLAGS.filter_tile):
 			with open(FLAGS.filter_tile) as fstat2:
@@ -114,35 +111,84 @@ def get_inference_from_file(TileName, stats_dict):
 		'''
 	else:
 		print("image not found in text file %s ... and that's weird..." % TileName)
-
-	print(oClass, current_score, (current_score-score_correction)/(1.0-score_correction))
+	#  print(oClass, current_score, (current_score-score_correction)/(1.0-score_correction))
 	#return oClass, (current_score-score_correction)/(1.0-score_correction)
 	return class_all
 
 
 
 def main():
-	# Read out_filename stats:
-	stats_dict = dict_tiles_stats()
-  
 
 	nClasses = [int(x) for x in FLAGS.Classes.split(',')]
 	print("nClasses: " + str(nClasses))
+
+	if FLAGS.threshold == '':
+		nThresh = ''
+	else:
+		nThresh = [float(x) for x in FLAGS.threshold.split(',')]
+		if len(nThresh) != len(nClasses):
+			sys.exit('the length of the threshold option must match the one of the class option')
+
+
+
+
 	SlideRootName = ''
 	SlideNames = []
-	idx = {}
+	idx = [{},{},{}]
 	iv1 = {}
 	iv2 = {}
 	iv3 = {}
+	ListSlideNames = {}
 	skip = False
+
+	# Read out_filename stats:
+	input_paths = FLAGS.tiles_stats.split(',')
+	stats_dict = [{},{},{}]
+	print(FLAGS.tiles_stats)
+	print(input_paths)
+	print(len(input_paths))
+	if len(input_paths) == 1:
+		stats_dict[0] = dict_tiles_stats(input_paths[0])
+		stats_dict[1] = stats_dict[0]
+		stats_dict[2] = stats_dict[0]
+	elif len(input_paths) == 3:
+		stats_dict[0] = dict_tiles_stats(input_paths[0])
+		stats_dict[1] = dict_tiles_stats(input_paths[1])
+		stats_dict[2] = dict_tiles_stats(input_paths[2])
+	elif len(input_paths) == 2:
+		if len(nClasses) != 2:
+			sys.exit('the number of input files does not match the number of classes used (2)!')
+		else:
+			stats_dict[0] = dict_tiles_stats(input_paths[0])
+			stats_dict[1] = dict_tiles_stats(input_paths[1])
+			stats_dict[2] = {}
+	else:
+		sys.exit('the number of input files does not match the number of classes used (2)!')
+
 
 	# print(stats_dict)
 	# print(FLAGS.slide_filter)
-	filtered_dict = {}
-	for k in stats_dict.keys():
+	filtered_dict = [{},{},{}]
+	for k in stats_dict[0].keys():
 		#print(k)
 		if FLAGS.slide_filter in k:
-			filtered_dict[k] = stats_dict[k]
+			filtered_dict[0][k] = stats_dict[0][k]
+	for k in stats_dict[1].keys():
+		#print(k)
+		if FLAGS.slide_filter in k:
+			filtered_dict[1][k] = stats_dict[1][k]
+	for k in stats_dict[2].keys():
+		if FLAGS.slide_filter in k:
+			filtered_dict[2][k] = stats_dict[2][k]
+
+
+
+	# for cChannel in range(3):
+	#	for tile in sorted(filtered_dict[cChannel].keys()):
+	#		cTileRootName =  '_'.join(tile.split('_')[0:-2]) 
+	#		ListSlideNames[cTileRootName] = []
+
+
 	# print("filtered:")
 	# print(filtered_dict)
 
@@ -152,86 +198,170 @@ def main():
 	Start = True
 	NewSlide = True
 	# For each image in the out_filename_stats:
-	for tile in sorted(filtered_dict.keys()):
-		# remove slide number from image name:
-		cTileRootName =  '_'.join(tile.split('_')[0:-2]) 
+	for cChannel in range(3):
+		print("Channel " + str(cChannel) + ", " + str(len(filtered_dict[cChannel].keys())) + " images")
+		# print(filtered_dict[cChannel])
+		tmp_count = 0
+		for tile in sorted(filtered_dict[cChannel].keys()):
+			tmp_count += 1
+			print(str(tmp_count) + " iterations of  " + str(cChannel))
+			# remove slide number from image name:
+			cTileRootName =  '_'.join(tile.split('_')[0:-2]) 
 
-		print("cTileRootName: %s" % cTileRootName)
-		if cTileRootName == '':
-			print("empty field")
-			continue
-		elif cTileRootName == SlideRootName:
-			if skip:
+			#print("cTileRootName: %s" % cTileRootName)
+			if cTileRootName == '':
+				print("empty field")
 				continue
+			elif cTileRootName == SlideRootName:
+				if skip:
+					continue
 
-			NewSlide = False
-			#if skip:
-			#	continue
-
-
-		else:
-			# Moved to a new slide
-			print("Analyzing %s" % (cTileRootName) )
-			idx[cTileRootName] = [(), ()]	
-			iv1[cTileRootName] = []	
-			iv2[cTileRootName] = []	
-			iv3[cTileRootName] = []	
-
-			#if skip:
+				NewSlide = False
+				#if skip:
 				#	continue
 
-			#elif skip==False:
-				# For previous the slide which is now finished, compute the averages 
 
-			NewSlide = True
-			skip = False
-		SlideRootName = cTileRootName
+			else:
+				# Moved to a new slide
+				# print("Analyzing %s" % (cTileRootName) )
+				# if cTileRootName in idx[0].keys() :
+				#	print("entry already in dictionary")
 				#else:
+				if cTileRootName not in idx[0].keys() :
+					idx[0][cTileRootName] = [(), ()]	
+					idx[1][cTileRootName] = [(), ()]	
+					idx[2][cTileRootName] = [(), ()]	
+					iv1[cTileRootName] = []	
+					iv2[cTileRootName] = []	
+					iv3[cTileRootName] = []	
+					ListSlideNames[cTileRootName] = []
+
+				#if skip:
+					#	continue
+
+				#elif skip==False:
+					# For previous the slide which is now finished, compute the averages 
+
+				NewSlide = True
+				skip = False
+			SlideRootName = cTileRootName
+					#else:
 
 
-		# extract coordinates of the tile
-		ixTile = int(tile.split('_')[-2])
-		iyTile = int(tile.split('_')[-1].split('.')[0])
+			# extract coordinates of the tile
+			ixTile = int(tile.split('_')[-2])
+			iyTile = int(tile.split('_')[-1].split('.')[0])
 
-		# check how big the "re-combined" slide should be (invert col/row because of the swapaxes required)
-		#rTile = im2.shape[1]
-		#cTile = im2.shape[0]
-					
-		#xTile =  (ixTile) * (FLAGS.tiles_size - FLAGS.tiles_overlap)
-		#yTile =  (iyTile) * (FLAGS.tiles_size - FLAGS.tiles_overlap)
-		#req_xLength = xTile + rTile
-		#req_yLength = yTile + cTile
+			# check how big the "re-combined" slide should be (invert col/row because of the swapaxes required)
+			#rTile = im2.shape[1]
+			#cTile = im2.shape[0]
+						
+			#xTile =  (ixTile) * (FLAGS.tiles_size - FLAGS.tiles_overlap)
+			#yTile =  (iyTile) * (FLAGS.tiles_size - FLAGS.tiles_overlap)
+			#req_xLength = xTile + rTile
+			#req_yLength = yTile + cTile
 
-		class_all = get_inference_from_file(tile, stats_dict)
+			class_all = get_inference_from_file(tile, stats_dict[cChannel])
 
-		# prepare heatmap
-		print("current score: " + str(class_all))
-		# print(idx, ixTile, iyTile)
-		idx[cTileRootName][0] += (iyTile,)
-		idx[cTileRootName][1] += (ixTile,)
-		if len(nClasses) > 0:
-			iv1[cTileRootName].append(class_all[nClasses[0]-1])
-		if len(nClasses) >1:
-			iv2[cTileRootName].append(class_all[nClasses[1]-1])
-		if len(nClasses) >2:
-			iv3[cTileRootName].append(class_all[nClasses[2]-1])
+			# prepare heatmap
+			# print("current score: " + str(class_all))
+			# print("nClasses[cChannel]: " + str(nClasses[cChannel]) + " cChannel: " + str(cChannel) + " class_all: " + str(class_all[nClasses[cChannel]-1]) + " iyTile: " +str(iyTile) + " ixTile: " + str(ixTile))
+			# print(idx, ixTile, iyTile)
+			if (len(nClasses) > 0) & (cChannel == 0) :
+				if nClasses[0] > 0:
+					if nThresh != '':
+						newProb = class_all[nClasses[0]-1] / nThresh[cChannel] * 0.5
+						if newProb > 0.5:
+							newProb = (newProb - 0.5) / (1.0 / nThresh[cChannel] - 1) + 0.5 
+						iv1[cTileRootName].append(newProb)
+						# print("old / new probability: " + str(class_all[nClasses[0]-1]) + " / " + str(newProb))
+					else:
+						iv1[cTileRootName].append(class_all[nClasses[0]-1])
+				else:
+					v1[cTileRootName].append(0)
+				idx[0][cTileRootName][0] += (iyTile,)
+				idx[0][cTileRootName][1] += (ixTile,)					
+			if (len(nClasses) > 1) & (cChannel == 1) :
+				if nClasses[1] > 0:
+					if nThresh != '':
+						newProb = class_all[nClasses[1]-1] / nThresh[cChannel] * 0.5
+						if newProb > 0.5:
+							newProb = (newProb - 0.5) / (1.0 / nThresh[cChannel] - 1) + 0.5 
+						iv2[cTileRootName].append(newProb)
+						# print("old / new probability: " + str(class_all[nClasses[1]-1]) + " / " + str(newProb))
+					else:
+						iv2[cTileRootName].append(class_all[nClasses[1]-1])
+				else:
+					iv2[cTileRootName].append(0)
+				idx[1][cTileRootName][0] += (iyTile,)
+				idx[1][cTileRootName][1] += (ixTile,)
+			if (len(nClasses) > 2) & (cChannel == 2) :
+				if nClasses[2] > 0:
+					if nThresh != '':
+						newProb = class_all[nClasses[2]-1] / nThresh[cChannel] * 0.5
+						if newProb > 0.5:
+							newProb = (newProb - 0.5) / (1.0 / nThresh[cChannel] - 1) + 0.5 
+						iv3[cTileRootName].append(newProb)
+						# print("old / new probability: " + str(class_all[nClasses[2]-1]) + " / " + str(newProb))
+					else:
+						iv3[cTileRootName].append(class_all[nClasses[2]-1])
+				else:
+					iv3[cTileRootName].append(0)
+				idx[2][cTileRootName][0] += (iyTile,)
+				idx[2][cTileRootName][1] += (ixTile,)
+			# print(iv1)
+			# print(idx)
 
-	for slide in idx.keys(): 
-		req_yLength = max(FLAGS.tiles_size, max(idx[slide][0])+1 )
-		req_xLength = max(FLAGS.tiles_size, max(idx[slide][1])+1 )
-		X1 = np.zeros([req_yLength,req_xLength])
-		print("idx, iv1:")
-		print(idx[slide])
-		print(len(idx[slide][0]), len(idx[slide][1]), len(iv1[slide]) )
-		print(req_yLength, req_xLength )
 
-		X1[idx[slide]] = iv1[slide]
-		X2 = np.zeros([req_yLength,req_xLength])
-		X3 = np.zeros([req_yLength,req_xLength])
-		if len(nClasses) >1:
-			X2[idx[slide]] = iv2[slide]
-		if len(nClasses) >2:
-			X3[idx[slide]] = iv3[slide]
+
+	# create heatmap
+	#for slide in idx[cChannel].keys(): 
+	# print(ListSlideNames)
+	# print(idx)
+	# print(idx[0])
+
+	for slide in ListSlideNames.keys(): 
+		print("slide: " + slide)
+		if len(idx[0][slide][0]) > 0:
+			M1y = max(idx[0][slide][0])+1
+			M1x = max(idx[0][slide][1])+1
+		else:
+			M1y = 0
+			M1x = 0
+		if len(idx[1][slide][0]) > 0:
+			M2y = max(idx[1][slide][0])+1
+			M2x = max(idx[1][slide][1])+1
+		else:
+			M2y = 0
+			M2x = 0
+		if len(idx[2][slide][0]) > 0:
+			M3y = max(idx[2][slide][0])+1
+			M3x = max(idx[2][slide][1])+1
+		else:
+			M3y = 0
+			M3x = 0
+		#req_yLength = max(FLAGS.tiles_size, max(idx[0][slide][0])+1, max(idx[1][slide][0])+1, max(idx[2][slide][0])+1 )
+		#req_xLength = max(FLAGS.tiles_size, max(idx[0][slide][1])+1, max(idx[1][slide][0])+1, max(idx[2][slide][0])+1 )
+		req_yLength = max(FLAGS.tiles_size, M1y, M2y, M3y )
+		req_xLength = max(FLAGS.tiles_size, M1x, M2x, M3x )
+		# print("lengths:" + str(req_yLength) + ", " + str(req_xLength))
+		for cChannel in range(3):
+			if cChannel ==0 :
+				X1 = np.zeros([req_yLength,req_xLength])
+				# print("idx, iv1:")
+				# print(idx[cChannel][slide])
+				# print(len(idx[cChannel][slide][0]), len(idx[cChannel][slide][1]), len(iv1[slide]) )
+				# print(req_yLength, req_xLength )
+
+				X1[idx[cChannel][slide]] = iv1[slide]
+			elif cChannel == 1 :
+				X2 = np.zeros([req_yLength,req_xLength])
+				if len(nClasses) >1 :
+					X2[idx[cChannel][slide]] = iv2[slide]
+			elif cChannel == 2 :
+				X3 = np.zeros([req_yLength,req_xLength])
+				if len(nClasses) >2:
+					X3[idx[cChannel][slide]] = iv3[slide]
 		rgb = np.zeros((req_yLength, req_xLength, 3), dtype=np.uint8)
 		rgb[..., 0] = X1 * 255
 		rgb[..., 1] = X2 * 255
@@ -273,7 +403,7 @@ if __name__ == '__main__':
       '--tiles_stats',
       type=str,
       default='',
-      help='text file where tile statistics are saved.'
+      help='text file where tile statistics are saved; if different files for different channel, sperate them with comma'
   )
   parser.add_argument(
       '--slide_filter',
@@ -291,7 +421,13 @@ if __name__ == '__main__':
       '--Classes',
       type=str,
       default=None,
-      help='Which classes to use  for each channel (up to 3; first class is 1, not 0) - string, for example: 2,1,4'
+      help='Which classes to use  for each channel (up to 3 numbers, one per channel; first class is 1, not 0 - use 0 if a channel should not be used) - string, for example: 2,1,4'
+  )
+  parser.add_argument(
+      '--threshold',
+      type=str,
+      default='',
+      help='apply color normalization such as threshold > 0.5 - no normalization if left empty.'
   )
 
   FLAGS, unparsed = parser.parse_known_args()
